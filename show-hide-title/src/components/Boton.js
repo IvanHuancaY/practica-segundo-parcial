131eb1522b0ff3b65c7796fb50df908af1d4d61f
IvanHuancaY <ivanyujra275@gmail.com>
2024-01-17 19:49:52 -0400

    17-01-2024

---
+++ show-hide-title/src/components/Boton.js
@@ -0,0 +1,15 @@
+import "../style/Boton.css"
+import { useState } from "react";
+
+function Boton() {
+    const [show, setShow] = useState(false);
+
+  return (
+    <div className="container">
+      <button onClick={() => setShow((show) => !show)}>  Hide / Show </button>
+      {show ? <h1>Welcome to React Challenges</h1> : null}
+    </div>
+  );
+}
+ 
+export default Boton;
