13704caa289d8cbe1d5044632fd2449071840867
IvanHuancaY <ivanyujra275@gmail.com>
2024-01-17 18:57:41 -0400

    17-01-2024

---
+++ ocultar-titulo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Boton  from './components/Boton'
+
+function App() {
+  return (
+    <div className="App">
+      <Boton />
+    </div>
+  );
+}
+
+export default App;
---
+++ ocultar-titulo/src/components/cont.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function cont({nroClicks}) {
+    return (
+        <div >
+            {nroClicks}
+        </div>
+    );
+}
+
+export default cont;
